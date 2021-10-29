133f43a510c58f95027fc0ef12f8371ed8a254ae
zhafiraak <zhafirakhafianti19@gmail.com>
2021-10-29 16:51:31 +0700

    menambahkan file

---
+++ luas_keliling_lingkaran.py
@@ -0,0 +1,11 @@
+print("Menghitung Luas dan Keliling Lingkaran")
+
+r = float(input("Masukan jari-jari lingkaran : "))
+
+phi      = 3.14
+luas     = phi*r*r
+keliling = 2*phi*r
+
+print ()
+print ("Luas Lingkaran :",format(luas,'.2f'))
+print ("Keliling Lingkaran :",format(keliling,'.2f'))
